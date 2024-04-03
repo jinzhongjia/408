2699a90c5089fd08e958515728e4df41d9b68b98
jinzhongjia <mail@nvimer.org>
2024-04-03 20:09:12 +0800

    完善sidebar

--- course/.vitepress/sidebar.ts
+++ course/.vitepress/sidebar.ts
@@ -1,22 +1,30 @@
 import { DefaultTheme } from "vitepress";
 
 export default [
-  {
-    text: "什么是 408 ？",
-    link: "/",
-  },
+	{
+		text: "什么是 408 ？",
+		link: "/",
+	},
 
-  {
-    text: "数据结构",
-    items: [
-      {
-        text: "绪论",
-        link: "/DataStructure/introduction",
-      },
-    ],
-  },
-  {
-    text: "计算机组成原理",
-    items: [],
-  },
+	{
+		text: "数据结构",
+		items: [
+			{
+				text: "绪论",
+				link: "/DataStructure/introduction",
+			},
+		],
+	},
+	{
+		text: "计算机组成原理",
+		items: [],
+	},
+	{
+		text: "计算机网络",
+		items: [],
+	},
+	{
+		text: "操作系统",
+		items: [],
+	},
 ] as DefaultTheme.Sidebar;
